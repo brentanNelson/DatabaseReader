4c4a6a091bc908908a9da30ccf24ad03f4b64c5c
brentanNelson <brentan.nelson@hotmail.com>
2017-08-29 12:07:05 +1000

    add price updater

---
+++ PriceUpdater/Models/UpdatedPriceData.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper.Configuration;
+
+namespace PriceUpdater.Models
+{
+    public class UpdatedPriceData
+    {
+        public string Product { get; set; }
+
+        public string CostP1 { get; set; }
+    }
+
+    public class UpdatedPriceDataMap : CsvClassMap<UpdatedPriceData>
+    {
+        public UpdatedPriceDataMap()
+        {
+            Map(m => m.Product);
+            Map(m => m.CostP1);
+        }
+    }
+}
---
+++ PriceUpdater/Program.cs
@@ -0,0 +1,84 @@
+﻿using CsvHelper;
+using MySql.Data.MySqlClient;
+using PriceUpdater.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var csvData = ReadCsv();
+            var update = UpdatePrices(csvData);
+        }
+
+        static List<UpdatedPriceData> ReadCsv()
+        {
+            Console.WriteLine($"Reading data from csv..");
+            var csvData = new List<UpdatedPriceData>();
+            using (var reader = new StreamReader(ConfigurationManager.AppSettings["InputFileLocation"]))
+            using (var csv = new CsvReader(reader))
+            {
+                csv.Configuration.HasHeaderRecord = true;
+                csv.Configuration.RegisterClassMap(new UpdatedPriceDataMap());
+
+                csvData.AddRange(csv.GetRecords<UpdatedPriceData>().ToList());
+            }
+            Console.WriteLine($"{csvData.Count()} records found.");
+            return csvData;
+        }
+
+        static bool UpdatePrices(List<UpdatedPriceData> newPriceData)
+        {
+            Console.WriteLine($"Updating database");
+            var totalUpdated = 0;
+            foreach (var priceData in newPriceData)
+            {
+                double parseResult;
+                Console.WriteLine($"Attempting to set {priceData.Product} to ${priceData.CostP1}");
+                if (string.IsNullOrWhiteSpace(priceData.Product) || Double.TryParse(priceData.CostP1, out parseResult))
+                {
+                    Console.Error.WriteLine($"Error: Invalid data in {priceData.Product} or {priceData.CostP1}, product must not be empty and costp1 should be a double value.");
+                    break;
+                }
+                
+                var query = $@"UPDATE prodsite,
+                           SET COSTP1 = '{priceData.CostP1}'
+                           WHERE PRODUCT = '{priceData.Product}'";
+
+                using (var connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString))
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    Console.WriteLine("Opening connection");
+                    try
+                    {
+                        connection.Open();
+                        var updatedRows = command.ExecuteNonQuery();
+                        totalUpdated += updatedRows;
+                        Console.WriteLine($"Successfully updated {updatedRows} rows.");
+                    }
+                    catch (MySqlException exSql)
+                    {
+                        Console.Error.WriteLine("Error - SQL Exception: " + query);
+                        Console.Error.WriteLine(exSql.StackTrace);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Error.WriteLine("Error - Exception: " + query);
+                        Console.Error.WriteLine(ex.StackTrace);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Successfully updated {totalUpdated} rows in total.");
+            return true;
+        }
+    }
+}
